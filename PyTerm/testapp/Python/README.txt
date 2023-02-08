5fd1172f726ea46857444fa82b39dc4a91119dd4
Dr. AIT <108739871+totallynotdrait@users.noreply.github.com>
2023-02-08 11:58:08 +0100

    Delete PyTerm directory

--- PyTerm/testapp/Python/README.txt
+++
@@ -1,1 +0,0 @@
-to run a Python script in PyTerm do 'python scriptname', to run a EXE file do 'open filename', Linux or MacOS users may got problems when doing this commands.
--- PyTerm/testapp/Python/Terminal/Python/main.py
+++
@@ -1,7 +0,0 @@
-print("""
--------------------------------------
-|   This is a app made in python    |                               
--------------------------------------
-""")
-
-print("DIR      'Directory'\nEXE      'Execute'\nMAKE     'Make'\nGET      'Get'\nTERM     'Term' or 'Terminal'\n")
